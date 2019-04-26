4784b67e922f3635ac6d4f692757758d895e4793
cin <lin@DESKTOP-JDLL057.localdomain>
2019-04-25 22:51:51 -0400

    implemented information gain

---
+++ Node.py
@@ -0,0 +1,5 @@
+class Node:
+    def __init__(self, entropy, datagroup):
+        self.leaves = list()
+        self.entropy = entropy
+        self.datagroup = datagroup
--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 import sys
 import numpy
-import pandas
+import pandas as pd
 
-def Entropy(A, B):
-    return A/B*numpy.log2(A/B)
-
-
-def cal_Entropy(dataset):
+def Entropy(attributes):
     S = 0
-    for result in dataset:
-        A = len(dataset.loc[[df['Survival'] == 1]])/len(dataset.index)      #size of yes type for dataset 
-        B = len(dataset.loc[[df['Survival'] == 2]])/len(dataset.index)
-        S -= Entropy(A, B)
+    total = float(sum(attributes))
+    for attr in attributes:
+        S += -1*(float(attr)/total)*(numpy.log2(float(attr)/total))
     return S
 
-def cal_InfoGain():
-    pass
+def InfoGain(dataset, overallEntropy):
+    attrGroup = dataset.groupby('attribute')
+    mlist = dataset['result'].unique()
+    gain = 0
+
+    for key, value in attrGroup:
+        attributes = list()
+        homo = False
+        for m in mlist:
+            A = value.loc[value['result'] == m]['result'].count()
+            if(A == 0):
+                homo = True       
+                break
+            attributes.append(A)
+        if(not homo): # zero entropy for homogenous sample
+            gain += value['result'].count()/dataset['result'].count() * Entropy(attributes)
+    return gain
+
+        # S.columns = ['freq']
+        # total = S['freq'].sum()
+        # print(S)
+        # print(total)
+        
+    
+
+    # for key, value in attrGroup:
+    #      A = value.loc[value['result'] == 'T']
+    #      print(A)
 
 # load the data
 '''
@@ -27,13 +48,13 @@ df = df.drop(columns='Name')
 df = df.drop(columns='Ticket')
 '''
 
-df = pandas.read_csv("haberman.data")
-df.columns = ["age", "operation", "pnodes", "Survival"]
+# df = pandas.read_csv("haberman.data")
+# df.columns = ["age", "operation", "pnodes", "Survival"]
 
-print(len(df['age'].unique()))
-for age in df['age'].unique():
-    # print(age)
-    print(age, df.groupby('age').get_group(age))
+# print(len(df['age'].unique()))
+# for age in df['age'].unique():
+#     # print(age)
+#     print(age, df.groupby('age').get_group(age))
 
 
 # for key, item in df.groupby("age"):
@@ -46,9 +67,9 @@ for age in df['age'].unique():
 #     print(key, item)
 
 
-df_1 = df[['age', 'Survival']]
-df_1_2 = df_1.loc[df['Survival'] == 1]
-df_1_1 = df_1.loc[df['Survival'] == 2]
+# df_1 = df[['age', 'Survival']]
+# df_1_2 = df_1.loc[df['Survival'] == 1]
+# df_1_1 = df_1.loc[df['Survival'] == 2]
 
 
 # df_1_2 = len((df.groupby('age').get_group(30).loc[df['Survival'] == 2]).index)
@@ -72,3 +93,52 @@ df_1_1 = df_1.loc[df['Survival'] == 2]
 #    print(age)
 
 #print(df['age'])
+
+
+nstretch = pd.read_csv('adult-stretch.data')
+nstretch.columns = ["color", "size", "mode", "person", "result"]
+resultColumn = nstretch.result
+nstretch_no_result = nstretch.drop('result', axis = 1)
+
+mlist = resultColumn.unique()
+attributes = list()
+for value in mlist:
+    S = resultColumn.loc[resultColumn == value].count()
+    print(type(S), S)
+    attributes.append(S)
+overallEntropy = Entropy(attributes)
+print(overallEntropy)
+
+
+for column in nstretch_no_result.columns:
+    print("==============================")
+    print(column)
+    dataset = nstretch[[column, 'result']]
+    dataset.columns = ["attribute", "result"]
+    infoGain = InfoGain(dataset, overallEntropy)
+    print(infoGain)
+
+    # S = cal_Entropy(group, column, 'result', mlist)
+    # print("Entropy({}): {}".format(column, S))
+
+# colorGroups = nstretch.groupby('color')
+# sizeGroups = nstretch.groupby('size')
+# modeGroups = nstretch.groupby('mode')
+# personGroups = nstretch.groupby('person')
+# mlist = ['T', 'F']
+# S = cal_Entropy(colorGroups, 'color', 'result', mlist)
+# print("Entropy(color): ", S)
+
+# S = cal_Entropy(sizeGroups, 'size', 'result', mlist)
+# print('Entropy(size): ', S)
+
+# S = cal_Entropy(modeGroups, 'mode', 'result', mlist)
+# print('Entropy(mode): ', S)
+
+# S = cal_Entropy(personGroups, 'person', 'result', mlist)
+# print('Entropy(person): ', S)
+
+
+# print(nstretch.groupby('color').get_group('YELLOW'))
+# print(nstretch.groupby(''))
+# print(pstretch)
